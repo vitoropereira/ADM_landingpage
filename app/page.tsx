62f95e5d40d02f5b2064b036a5373c948523f01a
Shinee Hawk <130590142+shinyhawk@users.noreply.github.com>
2023-07-25 05:29:45 -0400

    translation

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 // import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Blog from "@/components/Blog";
-import Brands from "@/components/Brands";
+// import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
 // import Features from "@/components/Features";
@@ -22,7 +22,7 @@ const Home = () => {
       <Hero />
       {/* <Features /> */}
       <Video />
-      <Brands />
+      {/* <Brands /> */}
       <AboutSectionOne />
       {/* <AboutSectionTwo /> */}
       <Testimonials />
--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -19,13 +19,13 @@ const Video = () => {
           mb="80px"
         />
 
-        {/* <div className="-mx-4 flex flex-wrap">
+        <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
             <div
               className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-md"
               data-wow-delay=".15s"
             >
-              <div className="relative aspect-[77/40] items-center justify-center">
+              {/* <div className="relative aspect-[77/40] items-center justify-center">
                 <Image src="/images/video/video.jpg" alt="video image" fill />
                 <div className="absolute top-0 right-0 flex h-full w-full items-center justify-center">
                   <button
@@ -42,10 +42,10 @@ const Video = () => {
                     </svg>
                   </button>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
-        </div> */}
+        </div>
       </div>
 
       <ModalVideo
